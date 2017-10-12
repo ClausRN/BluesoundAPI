9c6cb4f00a2c5865efc248f809607cc4cd0f7b54
Claus Rysgaard Nielsen <3144@grundfos.com>
2017-10-12 16:10:11 +0200

    Test

--- Controller.go
+++ Controller.go
@@ -2,11 +2,24 @@ package BluesoundAPI
 
 import (
 	"log"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"encoding/xml"
 )
 
 // NewBluesoundController is creating a new controller
-func NewBluesoundController() (blueControl BluesoundController) {
+func NewBluesoundController(FQDN string) (blueControl BluesoundController) {
 	blueControl.Name = ""
+	blueControl.baseURL = "http://" + FQDN + ":11000/"
+
+	if data, err := blueControl.getContent(blueControl.baseURL + "Status"); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		log.Println("Received XML:")
+		log.Println(string(data))
+	}
+	
 	return blueControl
 }
 
@@ -15,4 +28,32 @@ func (blueControl *BluesoundController) Play() bool {
 	blueControl.Name = "Play"
 	log.Println("Playing")
 	return true
+}
+
+// getContent return data from http request
+func (blueControl *BluesoundController) getContent(url string) ([]byte, error) {
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, fmt.Errorf("GET error: %v", err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != http.StatusOK {
+        return nil, fmt.Errorf("Status error: %v", resp.StatusCode)
+    }
+
+    data, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return nil, fmt.Errorf("Read body: %v", err)
+    }
+
+    return data, nil
+}
+
+func main(){
+	var XMLData string
+	XMLData = "'<?xml version=1.0 encoding=UTF-8 standalone=yes?><SyncStatus icon=/images/players/N110_nt.png volume=-1 modelName=NODE 2 name=Cave model=N110 brand=Bluesound etag=3 schemaVersion=15 syncStat=3 id=192.168.1.45:11000 mac=90:56:82:3F:AA:A0></SyncStatus>"
+	var MySyncStatus BluesoundSyncStatus
+	xml.Unmarshal([]byte(XMLData), &MySyncStatus)
+	fmt.Println("Name:" + MySyncStatus.Name)
 }
--- Structs.go
+++ Structs.go
@@ -1,17 +1,36 @@
 package BluesoundAPI
 
+
 // BluesoundStatus is Status for player
 type BluesoundStatus struct {
-	SongNo         int32
-	CurrentTrackNo int32
-	Songs          []string
+	Album		string
+	Artist		string
+	Name 		string
+	Quality		string
+	Length		int16
+	Second		int16
+	Artwork		string
+	Filename 	string
+	Repeat		string
+	Volume 		int16
+	Service 	string
+	State		string
+	Shuffle		string
 }
 
 // BluesoundSyncStatus is SyncStatus for player
 type BluesoundSyncStatus struct {
-	SongNo         int32
-	CurrentTrackNo int32
-	Songs          []string
+	Icon		string	`xml:"icon,attr"`
+	Volume 		int		`xml:"volume,attr"`
+	ModelName	string	`xml:"modelName,attr"`
+	Name		string	`xml:"name,attr"`
+	Model		string	`xml:"model,attr"`
+	Brand		string	`xml:"brand,attr"`
+	Localetag	int		`xml:"etag,attr"`
+	SchemaVersion	int	`xml:"schemaVersion,attr"`
+	SyncStat 	int		`xml:"syncStat,attr"`
+	ID			string	`xml:"id,attr"`
+	MAC			string	`xml:"mac,attr"`
 }
 
 // BluesoundController is the main controller object
@@ -19,4 +38,14 @@ type BluesoundController struct {
 	Status     BluesoundStatus
 	SyncStatus BluesoundSyncStatus
 	Name       string
+	baseURL    string
 }
+
+const(
+	BluesoundHTTPURI_Status		string 	= "Status"
+	Bluesound_ShuffleOn 		int16 	= 1
+	Bluesound_ShuffleOff 		int16 	= 0
+	Bluesound_RepeatAll			int16	= 0
+	Bluesound_RepeatTrack		int16	= 1
+	Bluesound_RepeatOff			int16	= 2
+)
