58fa12b5fc9f3a2a91bee35e38ad97017125294d
Claus Rysgaard Nielsen <3144@grundfos.com>
2017-10-19 21:12:20 +0200

    Basic functionality in place

--- Controller.go
+++ Controller.go
@@ -1,11 +1,15 @@
 package BluesoundAPI
 
 import (
-	"log"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-//	"encoding/xml"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
 )
 
 // NewBluesoundController is creating a new controller
@@ -13,40 +17,242 @@ func NewBluesoundController(FQDN string) (blueControl BluesoundController) {
 	blueControl.Name = ""
 	blueControl.baseURL = "http://" + FQDN + ":11000/"
 
-	if data, err := blueControl.getContent(blueControl.baseURL + "Status"); err != nil {
-		log.Printf("Failed to get XML: %v", err)
-	} else {
-		log.Println("Received XML:")
-		log.Println(string(data))
-	}
-	
+	blueControl.stopSignal = make(chan os.Signal, 1)
+	signal.Notify(blueControl.stopSignal, os.Interrupt, os.Kill)
+	defer signal.Stop(blueControl.stopSignal)
+
 	return blueControl
 }
 
-// Play starts the player from current track
-func (blueControl *BluesoundController) Play() bool {
-	blueControl.Name = "Play"
-	log.Println("Playing")
+// Start stops the internal updates
+func (blueControl *BluesoundController) Start() bool {
+	log.Println("About to start")
+	go blueControl.updateData()
 	return true
 }
 
-// getContent return data from http request
-func (blueControl *BluesoundController) getContent(url string) ([]byte, error) {
-    resp, err := http.Get(url)
-    if err != nil {
-        return nil, fmt.Errorf("GET error: %v", err)
-    }
-    defer resp.Body.Close()
-
-    if resp.StatusCode != http.StatusOK {
-        return nil, fmt.Errorf("Status error: %v", resp.StatusCode)
-    }
-
-    data, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return nil, fmt.Errorf("Read body: %v", err)
-    }
-
-    return data, nil
+// Close stops the internal updates
+func (blueControl *BluesoundController) Close() bool {
+	log.Println("About to stop")
+	close(blueControl.stopSignal)
+	log.Println("Thread signaled")
+	return true
 }
 
+// Status returns current status of player
+func (blueControl *BluesoundController) Status() BluesoundStatus {
+	statusUpdate.RLock()
+	temp := blueControl.status
+	statusUpdate.RUnlock()
+	return temp
+}
+
+// SyncStatus returns current status of player
+func (blueControl *BluesoundController) SyncStatus() BluesoundSyncStatus {
+	statusUpdate.RLock()
+	temp := blueControl.syncStatus
+	statusUpdate.RUnlock()
+	return temp
+}
+
+// Play starts the player from current track
+func (blueControl *BluesoundController) Play() (State BluesoundCommandState) {
+	return blueControl.sendCommandPlayPause(bluesoundHTTPURIPlay)
+}
+
+// Pause the player at current position
+func (blueControl *BluesoundController) Pause() (State BluesoundCommandState) {
+	return blueControl.sendCommandPlayPause(bluesoundHTTPURIPause)
+}
+
+// Skip to next track
+func (blueControl *BluesoundController) Skip() (State BluesoundCommandStateSkipBack) {
+	return blueControl.sendCommandSkipBack(bluesoundHTTPURISkip)
+}
+
+// Back to start or previous track
+func (blueControl *BluesoundController) Back() (State BluesoundCommandStateSkipBack) {
+	return blueControl.sendCommandSkipBack(bluesoundHTTPURIBack)
+}
+
+// sendCommandPlayPause sends a simple command to the player
+func (blueControl *BluesoundController) sendCommandPlayPause(Command string) (State BluesoundCommandState) {
+	playstate := BluesoundCommandState{}
+	playstate.State = "No State"
+	if XMLDataBin, err := blueControl.getContent(Command); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		// log.Printf("Received XML answer: %s", string(XMLDataBin))
+		err = xml.Unmarshal(XMLDataBin, &playstate)
+		if err != nil {
+			log.Printf("XML data not valid: %s", err)
+		}
+	}
+	log.Printf("Player command: %s - state: %s", Command, playstate.State)
+	return playstate
+}
+
+// sendCommandSkipBack sends a simple command to the player
+func (blueControl *BluesoundController) sendCommandSkipBack(Command string) (State BluesoundCommandStateSkipBack) {
+	playstate := BluesoundCommandStateSkipBack{}
+	if XMLDataBin, err := blueControl.getContent(Command); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		// log.Printf("Received XML answer: %s", string(XMLDataBin))
+		err = xml.Unmarshal(XMLDataBin, &playstate)
+		if err != nil {
+			log.Printf("XML data not valid: %s", err)
+		}
+	}
+	log.Printf("Player command: %s - ID: %d", Command, playstate.TrackID)
+	return playstate
+}
+
+// Clear the playlist
+func (blueControl *BluesoundController) Clear() (State bool) {
+	var success = false
+	playstate := BluesoundPlayQueue{}
+	if XMLDataBin, err := blueControl.getContent("Clear"); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		// log.Printf("Received XML answer: %s", string(XMLDataBin))
+		err = xml.Unmarshal(XMLDataBin, &playstate)
+		if err != nil {
+			log.Printf("XML data not valid: %s", err)
+		} else {
+			if playstate.Length == 0 {
+				success = true
+			}
+		}
+	}
+	return success
+}
+
+// GetPlaylists get playlist from player
+func (blueControl *BluesoundController) GetPlaylists() (State BluesoundPlaylists) {
+	playstate := BluesoundPlaylists{}
+	if XMLDataBin, err := blueControl.getContent("Playlists"); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		// log.Printf("Received XML answer: %s", string(XMLDataBin))
+		err = xml.Unmarshal(XMLDataBin, &playstate)
+		if err != nil {
+			log.Printf("XML data not valid: %s", err)
+		}
+	}
+	return playstate
+}
+
+// GetPlaylist get playlist from player
+func (blueControl *BluesoundController) GetPlaylist(Playlist string) (State BluesoundPlaylist) {
+	playstate := BluesoundPlaylist{}
+	if XMLDataBin, err := blueControl.getContent("Songs?playlist=" + url.QueryEscape(Playlist)); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		// log.Printf("Received XML answer: %s", string(XMLDataBin))
+		err = xml.Unmarshal(XMLDataBin, &playstate)
+		if err != nil {
+			log.Printf("XML data not valid: %s", err)
+		}
+	}
+	return playstate
+}
+
+// GetPlayQueue get playlist from player
+func (blueControl *BluesoundController) GetPlayQueue() (State BluesoundPlayQueue) {
+	playstate := BluesoundPlayQueue{}
+	if XMLDataBin, err := blueControl.getContent("Playlist"); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		// log.Printf("Received XML answer: %s", string(XMLDataBin))
+		err = xml.Unmarshal(XMLDataBin, &playstate)
+		if err != nil {
+			log.Printf("XML data not valid: %s", err)
+		}
+	}
+	return playstate
+}
+
+// PlayPlaylist starts playing the playlist
+func (blueControl *BluesoundController) PlayPlaylist(Playlist string) (State BluesoundAddSong) {
+	playstate := BluesoundAddSong{}
+	if XMLDataBin, err := blueControl.getContent("Add?playlist=" + url.QueryEscape(Playlist) + "&playnow=-1"); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+	} else {
+		// log.Printf("Received XML answer: %s", string(XMLDataBin))
+		err = xml.Unmarshal(XMLDataBin, &playstate)
+		if err != nil {
+			log.Printf("XML data not valid: %s", err)
+		}
+	}
+	return playstate
+}
+
+// getContent return data from http request
+func (blueControl *BluesoundController) getContent(CommandURL string) ([]byte, error) {
+	resp, err := http.Get(blueControl.baseURL + CommandURL)
+	if err != nil {
+		return nil, fmt.Errorf("GET error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Status error: %v", resp.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Read body: %v", err)
+	}
+
+	return data, nil
+}
+
+// updateData updates the values for status and syncstatus
+func (blueControl *BluesoundController) updateData() {
+	var Running = true
+	var StatusType int
+	var StatusURL string
+	var pause time.Duration
+	for Running {
+		log.Println("Updating data")
+		switch StatusType {
+		case 0:
+			StatusURL = bluesoundHTTPURIStatus
+		case 1:
+			StatusURL = bluesoundHTTPURISyncStatus
+		}
+		XMLDataBin, err := blueControl.getContent(StatusURL)
+		if err != nil {
+			log.Printf("Could not get %s: %s", StatusURL, err)
+		} else {
+			statusUpdate.Lock()
+			//			err = xml.Unmarshal(XMLDataBin, &blueControl.status)
+			switch StatusType {
+			case 0:
+				err = xml.Unmarshal(XMLDataBin, &blueControl.status)
+			case 1:
+				err = xml.Unmarshal(XMLDataBin, &blueControl.syncStatus)
+			}
+			statusUpdate.Unlock()
+			if err != nil {
+				log.Printf("XML data not valid for %s: %s", StatusURL, err)
+			}
+		}
+		StatusType++
+		if StatusType > 1 {
+			StatusType = 0
+			pause = 500
+		}
+		select {
+		case <-time.After(pause * time.Millisecond):
+			if 1 == 0 {
+				log.Println("Dummy")
+			}
+		case <-blueControl.stopSignal:
+			log.Println("Stop signal received, exiting!")
+			Running = false
+		}
+	}
+	log.Println("Stopped")
+}
--- Structs.go
+++ Structs.go
@@ -1,54 +1,143 @@
 package BluesoundAPI
 
-import(
+import (
 	"encoding/xml"
+	"os"
+	"sync"
 )
+
 // BluesoundStatus is Status for player
 type BluesoundStatus struct {
-	XMLName 		xml.Name	`xml:"Status"`
-	Album			string		`xml:"album"`
-	Artist			string
-	Name 			string
-	Quality			string
-	Length			int16
-	Second			int16
-	Artwork			string
-	Filename	 	string
-	Repeat			string
-	Volume 			int16
-	Service 		string
-	State			string
-	Shuffle			string
+	XMLName  xml.Name `xml:"status"`
+	Album    string   `xml:"album"`
+	Artist   string   `xml:"artist"`
+	Name     string   `xml:"name"`
+	Quality  string   `xml:"quality"`
+	Format   string   `xml:"streamformat"`
+	Length   int16    `xml:"totlen"`
+	Second   int16    `xml:"secs"`
+	Artwork  string   `xml:"image"`
+	Filename string   `xml:"fn"`
+	Repeat   string   `xml:"repeat"`
+	Volume   int16    `xml:"volume"`
+	Service  string   `xml:"service"`
+	State    string   `xml:"state"`
+	Shuffle  string   `xml:"shuffle"`
+	SongNo   int      `xml:"song"`
 }
 
 // BluesoundSyncStatus is SyncStatus for player
 type BluesoundSyncStatus struct {
-	XMLName 		xml.Name	`xml:"SyncStatus"`
-	Icon			string		`xml:"icon,attr"`
-	Volume 			int			`xml:"volume,attr"`
-	ModelName		string		`xml:"modelName,attr"`
-	Name			string		`xml:"name,attr"`
-	Model			string		`xml:"model,attr"`
-	Brand			string		`xml:"brand,attr"`
-	SchemaVersion	int			`xml:"schemaVersion,attr"`
-	SyncStat 		int			`xml:"syncStat,attr"`
-	ID				string		`xml:"id,attr"`
-	MAC				string		`xml:"mac,attr"`
+	XMLName       xml.Name `xml:"SyncStatus"`
+	Icon          string   `xml:"icon,attr"`
+	Volume        int      `xml:"volume,attr"`
+	ModelName     string   `xml:"modelName,attr"`
+	Name          string   `xml:"name,attr"`
+	Model         string   `xml:"model,attr"`
+	Brand         string   `xml:"brand,attr"`
+	SchemaVersion int      `xml:"schemaVersion,attr"`
+	SyncStat      int      `xml:"syncStat,attr"`
+	ID            string   `xml:"id,attr"`
+	MAC           string   `xml:"mac,attr"`
+}
+
+// BluesoundCommandState Returned by player command request
+type BluesoundCommandState struct {
+	XMLName xml.Name `xml:"state"`
+	State   string   `xml:",chardata"`
+}
+
+// BluesoundCommandStateSkipBack Returned by player command request
+type BluesoundCommandStateSkipBack struct {
+	XMLName xml.Name `xml:"id"`
+	TrackID int32    `xml:",chardata"`
+}
+
+// BluesoundPlaylistNames Returned by player command request
+type BluesoundPlaylistNames struct {
+	//XMLName xml.Name `xml:"name"`
+	Artwork string `xml:"image,attr"`
+	Name    string `xml:",chardata"`
+}
+
+// BluesoundPlaylists Returned by player command request
+type BluesoundPlaylists struct {
+	XMLName   xml.Name                 `xml:"playlists"`
+	Service   string                   `xml:"service,attr"`
+	Playlists []BluesoundPlaylistNames `xml:"name"`
+}
+
+// BluesoundTrackName Returned by player command request
+type BluesoundTrackName struct {
+	//XMLName xml.Name `xml:"name"`
+	AlbumID  int32  `xml:"albumid,attr"`
+	ArtistID int32  `xml:"artistid,attr"`
+	SongID   string `xml:"songid,attr"`
+	Service  string `xml:"service,attr"`
+	Artist   string `xml:"art"`
+	Album    string `xml:"alb"`
+	Title    string `xml:"title"`
+	Filename string `xml:"fn"`
+	Length   int32  `xml:"time"`
+	TrackNo  int32  `xml:"track"`
+	DiscNo   int32  `xml:"discno"`
+	Quality  string `xml:"quality"`
+}
+
+// BluesoundPlaylist Returned by player command request
+type BluesoundPlaylist struct {
+	XMLName xml.Name             `xml:"songs"`
+	Service string               `xml:"service,attr"`
+	ID      int32                `xml:"id,attr"`
+	Tracks  []BluesoundTrackName `xml:"song"`
+}
+
+// BluesoundAddSong Returned by player command request
+type BluesoundAddSong struct {
+	XMLName xml.Name `xml:"addsong"`
+	Added   int32    `xml:"count,attr"`
+	Total   int32    `xml:"length,attr"`
+	ID      int32    `xml:"id,attr"`
+}
+
+// BluesoundPlayQueue Returned by player command request
+type BluesoundPlayQueue struct {
+	XMLName  xml.Name             `xml:"playlist"`
+	Name     string               `xml:"name,attr"`
+	Modified int32                `xml:"modified,attr"`
+	Length   int32                `xml:"length,attr"`
+	ID       int32                `xml:"id,attr"`
+	Tracks   []BluesoundTrackName `xml:"song"`
 }
 
 // BluesoundController is the main controller object
 type BluesoundController struct {
-	Status     BluesoundStatus
-	SyncStatus BluesoundSyncStatus
+	status     BluesoundStatus
+	syncStatus BluesoundSyncStatus
+	stopSignal chan os.Signal
 	Name       string
 	baseURL    string
 }
 
-const(
-	BluesoundHTTPURI_Status		string 	= "Status"
-	Bluesound_ShuffleOn 		int16 	= 1
-	Bluesound_ShuffleOff 		int16 	= 0
-	Bluesound_RepeatAll			int16	= 0
-	Bluesound_RepeatTrack		int16	= 1
-	Bluesound_RepeatOff			int16	= 2
+var (
+	statusUpdate sync.RWMutex
+)
+
+const (
+	bluesoundHTTPURIStatus     string = "Status"
+	bluesoundHTTPURISyncStatus string = "SyncStatus"
+	bluesoundHTTPURIPlay       string = "Play"
+	bluesoundHTTPURIPause      string = "Pause"
+	bluesoundHTTPURIBack       string = "Back"
+	bluesoundHTTPURISkip       string = "Skip"
+	// BluesoundShuffleOn Shuffle is on
+	BluesoundShuffleOn int16 = 1
+	// BluesoundShuffleOff Shuffle is off
+	BluesoundShuffleOff int16 = 0
+	// BluesoundRepeatAll Repeat all tracks
+	BluesoundRepeatAll int16 = 0
+	// BluesoundRepeatTrack Repeat single track
+	BluesoundRepeatTrack int16 = 1
+	// BluesoundRepeatOff Repeat is off
+	BluesoundRepeatOff int16 = 2
 )
